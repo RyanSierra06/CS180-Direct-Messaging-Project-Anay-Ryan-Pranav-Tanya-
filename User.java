a1f3491f94dccba98ffc26b192e43987087ecd37
RyanSierra06 <rsierra@purdue.edu>
2024-10-29 21:34:30 -0400

    Added more to the user class

--- User.java
+++ User.java
@@ -1,30 +1,48 @@
 public class User {
     ArrayList<User> BlockedUsers;
     ArrayList<Message> thisUsersMessages;
-    ArrayList<Message> othterUsersMessages;
+    ArrayList<Message> otherUsersMessages;
+    ArrayList<User> friends;
 
     public User() {
         blockedUsers = new ArrayList<User>();
         messages = new ArrayList<Message>();
-        othterUsersMessages = new ArrayList<Message>();
+        otherUsersMessages = new ArrayList<Message>();
+        friends = new ArrayList<User>();
     }
 
     public void sendMessage(Message message, User reciver) {
-        reciver.addOtherUsersMessages(message);
+        reciver.getOtherUsersMessages().add(message);
     }
 
-    public void addOtherUsersMessages(Message message) {
-        this.otherUsersMessages.add(message);
+    public ArrayList<Message> getOtherUsersMessages() {
+        return otherUsersMessages;
     }
-
 
     public void blockUser(User blockedUser) {
         blockedUsers.add(blockedUser);
     }
-    public void deleteMessage(User reciver) {
+
+    public void deleteMessage(User reciver, Message message) {
+        reciever.getOtherUsersMessages().remove(message);
+        this.thisUsersMessages.remove(message);
 
     }
+
+    public ArrayList<Message> getThisUsersMessages() {
+        return thisUsersMessages;
+    }
+
+    public ArrayList<User> getBlockedUsers() {
+        return BlockedUsers;
+    }
+
+    public ArrayList<User> getFriends() {
+        return friends;
+    }
+
     public void unblockUser(User previouslyBlockedUser) {
+        blockedUsers.remove(previouslyBlockedUser);
 
     }
     public void readMessages() {
@@ -39,14 +57,14 @@ public class User {
     public User findUser(String userName) {
 
     }
-    public void displayAnotherProfile(User person) {
+    public void displayAnotherProfile(User otherUser) {
 
     }
     public void addFriend(User newFriend) {
-
+        friends.add(newFriend);
     }
     public void removeFriend(User oldFriend) {
-
+        friends.remove(oldFriend);
     }
     public void modifyMessageRecivingLimit() {
 
