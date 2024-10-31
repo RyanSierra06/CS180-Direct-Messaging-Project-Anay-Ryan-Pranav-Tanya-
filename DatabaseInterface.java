b116987790891978d5ae9c031c62ab1851ce3545
Anay Misra <anaymisra@Anays-MacBook-Pro.local>
2024-10-31 18:33:38 -0400

    Implemented Database and the Interface

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -1,3 +1,20 @@
-public interface DatabaseInterface {
+import java.util.ArrayList;
 
+public interface DatabaseInterface {
+    // User management
+    boolean addUser(User user);
+    User getUser(String username);
+    boolean updateUser(User user);
+    boolean deleteUser(String username);
+
+    // Message management
+    boolean addMessage(Message message);
+    ArrayList<Message> getMessages(User user);
+    boolean deleteMessage(Message message);
+
+    // Friend and Block management
+    boolean addFriend(User user, User friend);
+    boolean removeFriend(User user, User friend);
+    boolean blockUser(User user, User blockedUser);
+    boolean unblockUser(User user, User blockedUser);
 }
