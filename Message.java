2fd0cd9d42b17c81af47be8c9bb14b6d336b1277
RyanSierra06 <rsierra@purdue.edu>
2024-11-01 12:01:46 -0400

    deleted unneeded methods and updated Message.java

--- Message.java
+++ Message.java
@@ -3,37 +3,51 @@ import java.uitl.*;
 public class Message implements MessageInterface {
     String type;
     User user;
-    String message;
+    String messageText;
+    String messageImage;
     ArrayList<User> userList; // switch these to a hashMap if theyre allowed
     ArrayList<String> messageList; //switch these to a hashMap if they're allowed
 
     public Message(User user, String type, String message) {
         this.user = user;
-        this.type = type;
-        this.message = message;
+        if (type.equalsIgnoreCase("text")) {
+            this.messageText = message; // will be the actual text message to be sent
+            this.messageImage = "";
+            this.type = type;
+        } else if (type.equalsIgnoreCase("image")) {
+            this.messageImage = message; // will be the jpg or img file in a string fom (basically the directory location)
+            this.messageText = "";
+            this.type = type;
+        } else {
+            this.messageImage = "";
+            this.messageText = "";
+            this.type = "Invalid Message Type";
+        }
         userList = new ArrayList<User>();
         messageList = new ArrayList<String>();
     }
 
-    public boolean validateMessageAndType(String type) {
-        if (type.equals("text")) {
-            this.message = message; // will be the actual text message to be sent
-        } else if (type.equalsIgnoreCase("image")) {
-            this.message = message; // will be the jpg or img file in a string fom (basically the directory location)
-        } else {
-            this.message = "Invalid Message Type";
+    public boolean assignToUser(String message, String type, User user) {
+        //basically your addMessage class
+        //since the index of both arrays will be the same, their user and message are stored at the same spot
+        if(checkMessageType(type)) {
+            messageList.add(message);
+        } else  {
+            messageList.add("Invalid Message Type");
         }
-    }
-
-
-    public boolean assignToUser(String message, User user) {
-        //since the index of both arrays will be the same, they're user and message are stored at the same spot
         userList.add(user);
-        messageList.add(message);
     }
 
-    public String getMessage() {
-        return message;
+    public boolean checkMessageType(String type) {
+        return type.equalsIgnoreCase("text") || type.equalsIgnoreCase("image"))
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public String getMessageImage() {
+        return messageImage;
     }
 
     public String getType() {
--- textMessage.java
+++
@@ -1,4 +0,0 @@
-public class textMessage extends message {
-
-
-}
