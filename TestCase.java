1aeafac724998a1fb110262442d6135f1d286ca5
tanyajain06 <jain778@purdue.edu>
2024-11-01 11:49:19 -0400

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -1,3 +1,19 @@
-public class TestCase {
+/**
+ * Phase 1 Test Class for Team Project
+ * 
+ * Tests the functionality of the User, Message, and Database classes.
+ * 
+ * @author Tanya Jain, lab sec 12
+ * 
+ * @version November 1, 2024
+ *
+ */
 
+
+public class TestCase {
+  public static void main(String[] args) {
+        testUserClass();
+        testMessageClass();
+        testDatabaseClass();
+    }
 }
