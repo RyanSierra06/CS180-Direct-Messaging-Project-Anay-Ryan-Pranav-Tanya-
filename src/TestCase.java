a84395d2f264a2ece119193370aedf40d507abc0
tanyajain06 <jain778@purdue.edu>
2024-11-02 20:04:10 -0400

    all Message class test cases

--- src/TestCase.java
+++ src/TestCase.java
@@ -21,50 +21,75 @@ public class TestCase {
     System.out.println("testing User class: ");
     
     // test for ability to create a new user storing the username and pw correctly
-    User test1 = new User("testUser", "testPassword");
-    if (test1.getUsername().equals("testUser") && test1.getPassword().equals("testPassword")) {
+    User userTest1 = new User("testUser", "testPassword");
+    if (userTest1.getUsername().equals("testUser") && userTest1.getPassword().equals("testPassword")) {
       System.out.println("test passed, user creation successful, username and password stored");
     } else {
       System.out.println("test failed, username or password does not match");
     }
 
     // test for ability to set and get profile description
-    test1.setProfileDescription("test profile");
-    if (test1.getProfileDescription().equals("test profile")) {
+    userTest1.setProfileDescription("test profile");
+    if (userTest1.getProfileDescription().equals("test profile")) {
       System.out.println("test passed, get and set profile description successful");
     } else {
       System.out.println("test failed, get and set profile description does not match");
     }
 
     //test for ability to add friends
-    test1.addFriend("friendUser");
-    if (test1.getFriends().contains("friendUser")) {
+    userTest1.addFriend("friendUser");
+    if (userTest1.getFriends().contains("friendUser")) {
       System.out.println("test passed, friend successfully added");
     } else {
       System.out.println("test failed, friend not added");
     }
 
     // test for ability to remove friends
-    test1.removeFriend("friendUser");
-    if (!test1.getFriends().contains("friendUser")) {
+    userTest1.removeFriend("friendUser");
+    if (!userTest1.getFriends().contains("friendUser")) {
       System.out.println("test passed, friend successfully removed");
     } else {
       System.out.println("test failed, friend not removed");
     }
 
     // test for ability to block a friend
-    test1.addFriend("blockFriendUser");
-    test1.blockFriend("blockFriendUser");
-    if (test1.getBlockedFriends().contains("blockFriendUser") && !test1.getFriends().contains("blockFriendUser")) {
+    userTest1.addFriend("blockFriendUser");
+    userTest1.blockFriend("blockFriendUser");
+    if (userTest1.getBlockedFriends().contains("blockFriendUser") && !userTest1.getFriends().contains("blockFriendUser")) {
       System.out.println("test passed, friend successfully blocked");
     } else {
       System.out.println("test failed, friend not blocked / friend still in friend list");
     }
   }
 
+  // TEST 2: testing message class
   private static void testMessageClass() {
-    System.out.println("Testing Message Class: ");
-    
+    System.out.println("testing Message class: ");
+
+    // test for ability to create message object with correctly set text and type
+    User userTest2 = new User("testUser", "testPassword");
+    Message messageTest1 = new Message(userTest2, "text", "hello world!");
+    if (messageTest1.getMessageText().equals("hello world!") && messageTest1.getType().equals("text") && messageTest1.getMessageImage().isEmpty()) {
+      System.out.println("test passed, text message successfully created");
+    } else {
+      System.out.println("test failed, text message not created");
+    }
+
+    // test for ability to create message object with correctly set image, image type, and verification of empty text
+    Message messageTest2 = new Message(userTest2, "image", "imageLink.png");
+    if (messageTest2.getMessageImage().equals("imageLink.png") && messageTest2.getType().equals("image") && messageTest2.getMessageTest().isEmpty()) {
+      System.out.println("test passed, image message successfully created");
+    } else {
+      System.out.println("test failed, image message not created");
+    }
+
+    // test for ability to verify an invalid message type
+    Message messageTest3 = new Message(userTest2, "audio", "soundLink.mp3");
+    if (messageTest3.getType().equals("invalid message type") && messageTest3.getMessageTest().isEmpty() && messageTest3.getMessageImage().isEmpty()) {
+      System.out.println("test passed, invalid message type correctly handled");
+    } else {
+      System.out.println("test failed, invalid message type incorrectly handled");
+    }
   }
 
   private static void testDatabaseClass() {
