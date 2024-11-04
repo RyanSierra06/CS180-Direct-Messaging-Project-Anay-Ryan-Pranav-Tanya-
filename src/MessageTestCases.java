76f4b489180cbb354e65d9c7f85a07e093a73f63
RyanSierra06 <rsierra@purdue.edu>
2024-11-03 21:21:17 -0500

    message test cases againFINAL

--- src/MessageTestCases.java
+++ src/MessageTestCases.java
@@ -17,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Tanya Jain
  * @version November 3, 2024
  */
+@RunWith(Enclosed.class)
 public class MessageTestCases {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(TestCase.class);
@@ -29,27 +30,22 @@ public class MessageTestCases {
         }
     }
 
-    public static class MessageTestCases {
-        System.out.println("testing Message class: ");
-        
+    public static class TestCase {
+
         @Test(timeout = 1000)
         // test for ability to create message object with correctly set text and type
-        public void testCreateTextMessage () {
+        public void testGetMessageText () {
             User userTest2 = new User("testUser", "testPassword");
             Message messageTest1 = new Message(userTest2, "text", "hello");
-            Assert.assertEquals("hello", messageTest1.getMessageText(), "test failed, text message does not match");
-            Assert.assertEquals("text", messageTest1.getType(), "test failed, text message type is not 'text'");
-            Assert.assertTrue(messageTest1.getMessageImage().isEmpty(), "test failed, message image not empty");
+            Assert.assertEquals("hello", messageTest1.getMessageText());
         }
 
         @Test(timeout = 1000)
         // test for ability to create message object with correctly set image, image type, and verification of empty text
-        public void testCreateImageMessage () {
+        public void testGetImageMessage () {
             User userTest2 = new User("testUser", "testPassword");
             Message messageTest2 = new Message(userTest2, "image", "imageLink.png");
-            Assert.assertEquals("imageLink.png", messageTest2.getMessageImage(), "test failed, image message does not match");
-            Assert.assertEquals("image", messageTest2.getType(), "test failed, text message type is not 'image'");
-            Assert.assertTrue(messageTest2.getMessageText().isEmpty(), "test failed, message text not empty");
+            Assert.assertEquals("imageLink.png", messageTest2.getMessageImage());
         }
 
         @Test(timeout = 1000)
@@ -57,10 +53,24 @@ public class MessageTestCases {
         public void testInvalidMessageType () {
             User userTest2 = new User("testUser", "testPassword");
             Message messageTest3 = new Message(userTest2, "audio", "soundLink.mp3");
-            Assert.assertEquals("invalid message type", messageTest3.getType(), "test failed, invalid message type incorrectly handled");
-            Assert.assertTrue(messageTest3.getMessageText().isEmpty(), "test failed, message text should be empty for invalid type");
-            Assert.assertTrue(messageTest3.getMessageImage().isEmpty(), "test failed, message image should be empty for invalid type");
+            Assert.assertEquals("Invalid Message Type", messageTest3.getType());
+        }
+
+
+        @Test(timeout = 1000)
+        public void testAssignToUser() {
+            User userTest2 = new User("testUser", "testPassword");
+            Message messageTest4 = new Message(userTest2, "text", "test message");
+            Assert.assertTrue(messageTest4.assignToUser(messageTest4.getMessageText(), "test", userTest2.getUsername()));
+        }
+
+        @Test(timeout = 1000)
+        public void testGetMainUser() {
+            User userTest2 = new User("testUser", "testPassword");
+            Message messageTest5 = new Message(userTest2, "text", "test message");
+            Assert.assertEquals(userTest2, messageTest5.getMainUser());
         }
     }
 }
+
 
