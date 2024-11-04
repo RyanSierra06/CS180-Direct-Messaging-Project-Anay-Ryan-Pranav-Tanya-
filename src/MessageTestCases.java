060ea8d67cb2f2bba3a568b8cf173f949679ca95
tanyajain06 <jain778@purdue.edu>
2024-11-03 20:36:32 -0500

    Update MessageTestCases.java

--- src/MessageTestCases.java
+++ src/MessageTestCases.java
@@ -15,117 +15,37 @@ import org.junit.runner.notification.Failure;
  * @version Mar 08, 2024
  */
 
-@RunWith(Enclosed.class)
-public class MessageTestCases {
+ private static void testMessageClass() {
+    System.out.println("testing Message class: ");
 
-    public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(TestCase.class);
-        if (result.wasSuccessful()) {
-            System.out.println("Excellent - Test ran successfully");
-        } else {
-            for (Failure failure : result.getFailures()) {
-                System.out.println(failure.toString());
-            }
-        }
+    @Test
+    // test for ability to create message object with correctly set text and type
+    public void testCreateTextMessage() {
+      User userTest2 = new User("testUser", "testPassword");
+      Message messageTest1 = new Message(userTest2, "text", "hello");
+      assertEquals("hello", messageTest1.getMessageText(), "test failed, text message does not match");
+      assertEquals("text", messageTest1.getType(), "test failed, text message type is not 'text'");
+      assertTrue(messageTest1.getMessageImage().isEmpty(), "test failed, message image not empty");
     }
 
-    public static class TestCase {
-        @Test(timeout = 1000)
-        public void programTestCase() {
-
-            User initialUser = new User("user1", "user1");
-            initialUser.setName("Dan");
-            initialUser.setProfileDescription("Hi I'm Dan");
-            initialUser.setProfilePicture("./files/Danpfp.jpg");
-            initialUser.setReceiveAnyone(true);
-
-            User secondUser = new User("user2", "user2");
-            secondUser.setName("Ben");
-            secondUser.setProfileDescription("Hi I'm ben");
-            secondUser.setProfilePicture("./files/benpfp.jpg");
-            secondUser.setReceiveAnyone(false);
-
-            Message msg = new Message(initialUser, "text", "hehehe");
-
-            boolean assignToUser = msg.assignToUser(msg.getMessageText(), "text", initialUser.getUsername());
-
-
-
-            boolean menatToBreak = initialUser.sendMessage(msg, secondUser.getUsername());
-
-            boolean addedFriend = secondUser.addFriend(initialUser.getUsername());
-            boolean addedFriend2 = initialUser.addFriend(secondUser.getUsername());
-            boolean meantToWork = initialUser.sendMessage(msg, secondUser.getUsername());
-
-
-            String user1Friends = initialUser.getFriends();
-
-            User thirdUser = new User("user3", "user3");
-            thirdUser.setName("Son");
-            thirdUser.setProfileDescription("Hi I'm son");
-            thirdUser.setProfilePicture("./files/sonpfp.jpg");
-            thirdUser.setReceiveAnyone(false);
-
-
-
-            boolean ableToBlock = initialUser.blockUser(thirdUser.getUsername());
-            String userOneblocks = initialUser.getBlockedUsers();
-            boolean removedBlock = initialUser.unblockUser(thirdUser.getUsername());
-            String userOnePostBlocks = initialUser.getBlockedUsers();
-
-            boolean nonExistentUser = initialUser.blockUser("nonexistent");
-
-            boolean removedFriend = initialUser.removeFriend(secondUser.getUsername());
-            String msgRead = initialUser.readMessages(secondUser.getUsername());
-            boolean msgRemoved = initialUser.deleteMessage(secondUser.getUsername(), msg);
-            String userFinder = initialUser.findUser(thirdUser.getUsername());
-
-            Assert.assertEquals("1", menatToBreak, false);
-            Assert.assertEquals("2",addedFriend, true);
-            Assert.assertEquals("3",addedFriend2, true);
-
-            Assert.assertEquals("4",meantToWork, true);
-            Assert.assertEquals("5",user1Friends, secondUser.getUsername());
-
-            Assert.assertEquals("6",ableToBlock, true);
-            Assert.assertEquals("7",userOneblocks, thirdUser.getUsername());
-            Assert.assertEquals("8",removedBlock, true);
-            Assert.assertEquals("9",userOnePostBlocks, "");
-
-
-            Assert.assertEquals("10",nonExistentUser, false);
-            Assert.assertEquals("11",removedFriend, true);
-            Assert.assertEquals("12",msgRead, "user1-user2-hehehe\n");
-            Assert.assertEquals("13", msgRemoved, true);
-            Assert.assertEquals("14",userFinder, "user3-Son-Hi I'm son-./files/sonpfp.jpg-false");
-
-            initialUser.blockUser(thirdUser.getUsername());
-            initialUser.addFriend(secondUser.getUsername());
-        }
-
-        @Test(timeout = 1000)
-        public void runDatabaseTestCases() {
-            User initialUser = new User("user1", "user1");
-            String  initialUserName = initialUser.getName();
-            String  initialUserPass = initialUser.getPassword();
-            String  initialUserProfDes = initialUser.getProfileDescription();
-            String  initialUserProfPic = initialUser.getProfilePicture();
-            String  initialUserFriends = initialUser.getFriends();
-            String  initialUserBlocked = initialUser.getBlockedUsers();
-
-            User secondUser = new User("user2", "user2");
-
-
-            String msg = initialUser.readMessages(secondUser.getUsername());
-
-            Assert.assertEquals("15", initialUserName, "Dan");
-            Assert.assertEquals("16", initialUserPass, "user1");
-            Assert.assertEquals("17", initialUserProfDes, "Hi I'm Dan");
-            Assert.assertEquals("18", initialUserProfPic, "./files/Danpfp.jpg");
-            Assert.assertEquals("19", initialUserFriends, secondUser.getUsername());
-            Assert.assertEquals("20", initialUserBlocked, "user3");
-            Assert.assertEquals("21", msg, "");
-        }
+    @Test
+    // test for ability to create message object with correctly set image, image type, and verification of empty text
+    public void testCreateImageMessage() {
+      User userTest2 = new User("testUser", "testPassword");
+      Message messageTest2 = new Message(userTest2, "image", "imageLink.png");
+      assertEquals("imageLink.png", messageTest2.getMessageImage(), "test failed, image message does not match");
+      assertEquals("image", messageTest2.getType(), "test failed, text message type is not 'image'");
+      assertTrue(messageTest2.getMessageText().isEmpty(), "test failed, message text not empty");
     }
 
+    @Test
+    // test for ability to verify an invalid message type
+    public void testInvalidMessageType() {
+      User userTest2 = new User("testUser", "testPassword");
+      Message messageTest3 = new Message(userTest2, "audio", "soundLink.mp3");
+      assertEquals("invalid message type", messageTest3.getType(), "test failed, invalid message type incorrectly handled");
+      assertTrue("messageTest3.getMessageText().isEmpty(), "test failed, message text should be empty for invalid type");
+      assertTrue("messageTest3.getMessageImage().isEmpty(), "test failed, message image should be empty for invalid type");
+    }
+  }
 }
