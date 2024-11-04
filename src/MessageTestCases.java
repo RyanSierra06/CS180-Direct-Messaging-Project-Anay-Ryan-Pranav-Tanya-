bdf3150a8d65e6f7e191b22097cb96e03c9aaacd
tanyajain06 <jain778@purdue.edu>
2024-11-03 20:58:36 -0500

    Update MessageTestCases.java

--- src/MessageTestCases.java
+++ src/MessageTestCases.java
@@ -18,10 +18,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @version November 3, 2024
  */
 public class MessageTestCases {
-    private static void testMessageClass() {
-        System.out.println("testing Message class: ");
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestCase.class);
+        if (result.wasSuccessful()) {
+            System.out.println("excellent - test ran successfully");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+    }
 
-        @Test
+    public static class MessageTestCases {
+        System.out.println("testing Message class: ");
+        
+        @Test(timeout = 1000)
         // test for ability to create message object with correctly set text and type
         public void testCreateTextMessage () {
             User userTest2 = new User("testUser", "testPassword");
@@ -31,7 +42,7 @@ public class MessageTestCases {
             assertTrue(messageTest1.getMessageImage().isEmpty(), "test failed, message image not empty");
         }
 
-        @Test
+        @Test(timeout = 1000)
         // test for ability to create message object with correctly set image, image type, and verification of empty text
         public void testCreateImageMessage () {
             User userTest2 = new User("testUser", "testPassword");
@@ -41,7 +52,7 @@ public class MessageTestCases {
             assertTrue(messageTest2.getMessageText().isEmpty(), "test failed, message text not empty");
         }
 
-        @Test
+        @Test(timeout = 1000)
         // test for ability to verify an invalid message type
         public void testInvalidMessageType () {
             User userTest2 = new User("testUser", "testPassword");
