e8dc47c06613afdb33c8c9f783694df1c0f96e7b
tanyajain06 <jain778@purdue.edu>
2024-11-08 12:40:54 -0500

    Update ApplicationServer.java

--- src/ApplicationServer.java
+++ src/ApplicationServer.java
@@ -1,4 +1,7 @@
 
-public class ApplicationServer {
-   //yo
+public class ApplicationServer implements Runnable {
+   //need interface
+   //information stored on server side
+   //information accessed via network IO using the client
+   
 }
