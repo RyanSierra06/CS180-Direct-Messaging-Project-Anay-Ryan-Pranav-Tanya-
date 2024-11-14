283d7115b9bba19ec94ff8534d2400e3ddf3a76b
tanyajain06 <jain778@purdue.edu>
2024-11-14 15:32:20 -0500

    Update ApplicationServer.java

--- src/ApplicationServer.java
+++ src/ApplicationServer.java
@@ -18,8 +18,26 @@ public class ApplicationServer implements ApplicationServerInterface {
    // information accessed via network IO using the client
 
    public static void main(String[] args) throws IOException {
-      // insert variables here
-      
-      // insert methods here
+      ServerSocket serverSocket = new ServerSocket(4242);
+      System.out.println("waiting for client to connect");
+      Socket socket = serverSocket.accept();
+      System.out.println("client connected");
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+      String message = reader.readLine();
+      System.out.printf("received from client:\n%s\n", message);
+
+      String response = message.replaceAll(" ", ",");
+      writer.write(response);
+      writer.println();
+      writer.flush();
+      System.out.printf("sent to client:\n%s\n", response);
+
+      writer.close();
+      reader.close();
+      socket.close();
+      serverSocket.close();
    }   
 }
