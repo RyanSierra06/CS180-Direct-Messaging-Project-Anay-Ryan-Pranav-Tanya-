1b085672897753f18bb243cbf44995096d508354
tanyajain06 <jain778@purdue.edu>
2024-11-14 15:56:04 -0500

    Update ApplicationServerInterface.java

--- src/ApplicationServerInterface.java
+++ src/ApplicationServerInterface.java
@@ -1,1 +1,13 @@
+/**
+ * Project 4 -- ApplicationInterface
+ *
+ * This is the ApplicationInterface interface
+ *
+ * @author Pranav Neti, Ryan Sierra, Tanya Jain, Anay Misra - Lab Section 12
+ *
+ * @version Nov 14, 2024
+ *
+ */
 
+public interface ApplicationServerInterface {
+}
